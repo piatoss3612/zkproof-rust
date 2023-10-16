2295998e99f614d3da31409b89bbb6640ff24643
Lee Hyohwak <crewe1746@naver.com>
2023-10-16 18:28:53 +0900

    Add zkp lib.rs

---
+++ zkp/src/lib.rs
@@ -0,0 +1,33 @@
+use num_bigint::BigUint;
+
+// n^exp mod p
+pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
+    n.modpow(exponent, modulus)
+}
+
+// s = k - c * x mod p
+pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint, p: &BigUint) -> BigUint {
+    if *k >= c * x {
+        (k - c * x).modpow(&BigUint::from(1u32), p)
+    } else {
+        p - (c * x - k).modpow(&BigUint::from(1u32), p)
+    }
+}
+
+// r1 = alpha^s * y1^c
+// r2 = beta^s * y2^c
+pub fn verify(
+    r1: &BigUint,
+    r2: &BigUint,
+    y1: &BigUint,
+    y2: &BigUint,
+    alpha: &BigUint,
+    beta: &BigUint,
+    c: &BigUint,
+    s: &BigUint,
+    p: &BigUint,
+) -> bool {
+    let cond1 = *r1 == alpha.modpow(s, p) * y1.modpow(c, p) % p;
+    let cond2 = *r2 == beta.modpow(s, p) * y2.modpow(c, p) % p;
+    cond1 && cond2
+}
