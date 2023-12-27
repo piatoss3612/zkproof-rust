43d06ed46a549e92f02eaebe7dc76284b61c4da1
piatoss <crewe1746@naver.com>
2023-12-27 09:26:06 +0900

    Create challenge and verify it on client

--- zkp/src/bin/client.rs
+++ zkp/src/bin/client.rs
@@ -1,7 +1,10 @@
 use std::io::stdin;
 
 use num_bigint::BigUint;
-use zkp::{zkp_auth::RegisterRequest, ZKP};
+use zkp::{
+    zkp_auth::{AuthenticationAnswerRequest, AuthenticationChallendgeRequest, RegisterRequest},
+    ZKP,
+};
 
 use crate::zkp::zkp_auth::auth_client::AuthClient;
 
@@ -30,11 +33,46 @@ async fn main() {
     let y2 = ZKP::exponentiate(&zkp.beta, &password, &zkp.p);
 
     let request = RegisterRequest {
-        user: username,
+        user: username.clone(),
         y1: y1.to_bytes_be(),
         y2: y2.to_bytes_be(),
     };
 
-    let response = client.register(request).await.unwrap();
-    println!("Response: {:?}", response);
+    let response = client.register(request).await.unwrap().into_inner();
+    println!("Register Response: {:?}", response);
+
+    let k = ZKP::generate_random_number(&zkp.q);
+    let r1 = ZKP::exponentiate(&zkp.alpha, &k, &zkp.p);
+    let r2 = ZKP::exponentiate(&zkp.beta, &k, &zkp.p);
+
+    let request = AuthenticationChallendgeRequest {
+        user: username.clone(),
+        r1: r1.to_bytes_be(),
+        r2: r2.to_bytes_be(),
+    };
+
+    let response = client
+        .create_authentication_challenge(request)
+        .await
+        .unwrap()
+        .into_inner();
+    println!("Authentication Challenge Response: {:?}", response);
+
+    let auth_id = response.auth_id;
+    let c = BigUint::from_bytes_be(&response.c);
+
+    let s = zkp.solve(&k, &c, &password);
+
+    let request = AuthenticationAnswerRequest {
+        auth_id,
+        s: s.to_bytes_be(),
+    };
+
+    let response = client
+        .verify_authentication(request)
+        .await
+        .unwrap()
+        .into_inner();
+
+    println!("Authentication Verification Response: {:?}", response);
 }
